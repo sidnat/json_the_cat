c8bde6cc72afdcb0e6c276af40dc9aed868b9792
Sid Natarajan <sid.nat@gmail.com>
2022-10-27 21:02:13 +0000

    created test cases using mocha

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,20 @@
-const breedName = process.argv[2];
 const request = require('request');
-
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-
-  if (error) {
-    return console.log(error)
-  }
   
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    return console.log("Breed not found.");
-  }
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+  
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      return callback("Breed not found.");
+    }
 
-  console.log(data[0].description);
-});
+    callback(null, data[0].description);
+  });
+};
+
+
+module.exports = { fetchBreedDescription };
+
---
+++ test/breedFetcherTest.js
@@ -0,0 +1,32 @@
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      // compare returned description
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+
+  it('returns "Breed not found." for a invalid breed, via callback', (done) => {
+    fetchBreedDescription('Siberrrrian', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(desc, null);
+
+      const expectedDesc = "Breed not found.";
+
+      // compare returned description
+      assert.equal(expectedDesc, err.trim());
+
+      done();
+    });
+  });
+});
